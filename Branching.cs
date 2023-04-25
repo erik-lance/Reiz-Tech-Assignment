84eaf6aa68b0178b46cb1e319298980fde64ee15
erik-lance <eriklance@gmail.com>
2023-04-25 13:58:05 +0800

    Added branch methods and changed to private

--- Branching.cs
+++ Branching.cs
@@ -1,6 +1,23 @@
 class Branch
 {
-    List<Branch> branches = new List<Branch>();
+    // A branch has a list of child branches
+
+    private List<Branch> branches = new List<Branch>();
+
+    public void AddBranch(Branch branch)
+    {
+        branches.Add(branch);
+    }
+
+    public List<Branch> GetBranches()
+    {
+        return branches;
+    }
+
+    public Branch()
+    {
+        branches = new List<Branch>();
+    }
 }
 
 class Branching
