8fda4d1d267a2fae0a2bfda189dd7b3bd905117a
erik-lance <eriklance@gmail.com>
2023-04-25 14:03:43 +0800

    Added depth calculator

--- Branching.cs
+++ Branching.cs
@@ -26,4 +26,33 @@ class Branching
     {
 
     }
+
+    /**
+     * Calculates the depth of a branch.
+     * 
+     * @param branch The branch to calculate the depth of.
+     * @return The depth of the branch.
+     */
+    int CalculateDepth(Branch branch)
+    {
+        if (branch.GetBranches().Count == 0)
+        {
+            // Base case: if this branch has no children, its depth is 1
+            return 1;
+        }
+        else
+        {
+            // Recursive case: the depth of this branch is 1 plus the maximum depth of its children
+            int maxChildDepth = 0;
+            foreach (Branch child in branch.GetBranches())
+            {
+                int childDepth = CalculateDepth(child);
+                if (childDepth > maxChildDepth)
+                {
+                    maxChildDepth = childDepth;
+                }
+            }
+            return 1 + maxChildDepth;
+        }
+    }
 }
