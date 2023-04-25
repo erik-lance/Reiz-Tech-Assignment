d386a4443476030e8a55354095e4b63d319f8d68
erik-lance <eriklance@gmail.com>
2023-04-25 12:46:29 +0800

    Added input

--- Program.cs
+++ Program.cs
@@ -8,10 +8,20 @@ class Program
 
     static void Main(string[] args)
     {
+        int hours = 0;
+        int minutes = 0;
+
         Console.WriteLine("Analog Clock Console App");
         Console.WriteLine("Erik Lance Tiongquico (2023 April)");
         Console.WriteLine();
 
+        Console.Write("Input the number of hours: ");
+        hours = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the number of minutes: ");
+        minutes = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
     }
 }
