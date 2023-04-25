c87bbd3be69f11be31cb00c728f5db529f7b6f16
erik-lance <eriklance@gmail.com>
2023-04-25 14:10:49 +0800

    Added branches based on diagram

--- Branching.cs
+++ Branching.cs
@@ -24,7 +24,48 @@ class Branching
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Branching Hierarchical Structure");
+        Console.WriteLine("Erik Lance Tiongquico (2023 April)");
+        Console.WriteLine();
 
+        // Create a tree of branches based on the diagram
+        Branch root = new Branch();
+
+        // Level 1
+        Branch level1Branch1 = new Branch();
+        Branch level1Branch2 = new Branch();
+
+        root.AddBranch(level1Branch1);
+        root.AddBranch(level1Branch2);
+
+        // Level 2
+        Branch level2Branch1 = new Branch();
+        Branch level2Branch2 = new Branch();
+        Branch level2Branch3 = new Branch();
+        Branch level2Branch4 = new Branch();
+
+        level1Branch1.AddBranch(level2Branch1);
+        level1Branch1.AddBranch(level2Branch2);
+        level1Branch1.AddBranch(level2Branch3);
+        level1Branch2.AddBranch(level2Branch4);
+
+        // Level 3
+        Branch level3Branch1 = new Branch();
+        Branch level3Branch2 = new Branch();
+        Branch level3Branch3 = new Branch();
+
+        level2Branch2.AddBranch(level3Branch1);
+        level2Branch2.AddBranch(level3Branch2);
+        level2Branch3.AddBranch(level3Branch3);
+
+        // Level 4
+        Branch level4Branch1 = new Branch();
+
+        level3Branch2.AddBranch(level4Branch1);
+
+        // Calculate the depth of the tree
+        int depth = CalculateDepth(root);
+        Console.WriteLine("The depth of the tree is: " + depth);
     }
 
     /**
@@ -33,7 +74,7 @@ class Branching
      * @param branch The branch to calculate the depth of.
      * @return The depth of the branch.
      */
-    int CalculateDepth(Branch branch)
+    static int CalculateDepth(Branch branch)
     {
         if (branch.GetBranches().Count == 0)
         {
