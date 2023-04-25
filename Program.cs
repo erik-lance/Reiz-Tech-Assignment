b101384a7cdd53ae8b2f0477a1dc2e4b2a28895c
erik-lance <eriklance@gmail.com>
2023-04-25 12:14:35 +0800

    Added initial details

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Branch
+{
+    List<Branch> branches = new List<Branch>();
+}
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Analog Clock Console App");
+        Console.WriteLine("Erik Lance Tiongquico (2023 April)");
+        Console.WriteLine();
+
+        Console.WriteLine("Press any key to exit");
+    }
+}
